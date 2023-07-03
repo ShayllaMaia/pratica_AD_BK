a26d198dc7dd48d7d8809fff308e585ee7ab6ecb
Shailla Maria da Silva Maia <shailla.maia@sou.ufac.br>
2023-07-03 18:12:53 -0300

    Prática 1

---
+++ project-favorites/public/scripts.js
@@ -0,0 +1,68 @@
+const ul = document.querySelector('ul')
+const input = document.querySelector('input')
+const form = document.querySelector('form')
+
+/*
+
+// Não se preocupem com esse pedaço de código comentado! Vamos descomentá-lo quando tivermos acabado de construir a API.
+
+// Função que carrega o conteúdo da API.
+async function load() {
+    // fetch está como await para evitar que entre num esquema de promisse e só devolva o conteúdo após a iteração qua acontece em seguida.
+    const res = await fetch('http://localhost:3000/')
+        .then(data => data.json())
+    // Iterando no vetor com o conteúdo (JSON) que está vindo da API e adicionando-os no frontend.
+    res.urls.map(({name, url}) => addElement({name, url}))
+}
+
+load()
+*/
+
+function addElement({ name, url }) {
+    const li = document.createElement('li');
+    li.innerHTML = 
+    `
+        <a href="${url}" target="_blank">${name}</a>
+        <button id="remover">Remover</button>
+    `;
+
+    li.querySelector('button').addEventListener('click', () => {
+        removeElement(li);
+    });
+    
+    ul.appendChild(li);
+}
+
+
+function removeElement(element) {
+    const confirmar = confirm("Deseja realmente remover esse link?")
+    if (confirmar){
+        element.remove();
+    }
+}
+
+
+
+
+form.addEventListener('submit', (event) => {
+    
+    event.preventDefault();
+
+    let { value } = input
+
+    if (!value)
+        return alert('Preencha o campo!')
+
+    const [name, url] = value.split(',')
+
+    if (!url)
+        return alert('O texto não está formatado da maneira correta.')
+
+    if (!/^http/.test(url))
+        return alert('Digite a url da maneira correta.')
+
+    addElement({ name, url })
+
+    input.value = ''
+
+})
